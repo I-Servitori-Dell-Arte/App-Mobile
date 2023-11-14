74f3f434a85692b91c396d56452ffe16143f4071
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2023-11-14 15:41:25 +0100

    ok

--- client/constants/Network.js
+++ client/constants/Network.js
@@ -1,5 +1,5 @@
 export default network = {
-  serverip: "https://1d74-2001-b07-a72-213b-c010-3123-5744-d90d.ngrok-free.app",
+  serverip: "https://servitoridellarte.up.railway.app",
 };
-//https://api-easybuy.herokuapp.com
+//https://servitoridellarte.up.railway.app
 //http://192.168.1.195:8080
